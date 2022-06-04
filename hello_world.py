842200922204d8a611526a7c2c074ee21078c5d2
Endrit Isufi <endrit.isufi@tuni.fi>
2022-06-04 15:27:42 +0300

    delete comments

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,1 @@
-"""
-Info: # TIE-GIT git-kurssi / git course
-Name: Endrit Isufi
-Student ID: 050023710
-"""
-
-def main():
-    print("Hello World!")
-
-if __name__ == '__main__':
-    main()
+print("Hello World!")
